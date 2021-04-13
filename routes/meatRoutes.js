56ec10f8b571f6852f14772a6c64fe27d41a9cdd
David Hall <davidchall388@gmail.com>
2021-04-13 12:08:14 -0400

    add product post working

--- routes/meatRoutes.js
+++ routes/meatRoutes.js
@@ -24,12 +24,45 @@ meatRouter.get("/:category", async (req,res) => {
 })
 
     meatRouter.put("/:id", async (req,res) => {
-        var meat = req.body;
-
-        console.log(meat)
+        console.log(req.params.id)
+        console.log(req.body)
+        Meat.findByIdAndUpdate(
+            // the id of the item to find
+            req.params.id,
+            
+            // the change to be made. Mongoose will smartly combine your existing 
+            // document with this change, which allows for partial updates too
+            req.body,
+            
+            // an option that asks mongoose to return the updated version 
+            // of the document instead of the pre-updated one.
+            {new: true},
+            
+            // the callback function
+            (err, meat) => {
+            // Handle any possible database errors
+                if (err) return res.status(500).send(err);
+                return res.send(meat);
+            }
+        )
         
 
         // company = _.extend(company, req.body);
+    })
+
+    meatRouter.post("/", async (req,res) => {
+        const product = req.body
+
+        console.log(product)
+        await new Meat({
+            name: product.name,
+            price: product.price,
+            unit: product.unit,
+            description: product.description,
+            popularity: 0,
+            pictures:   product.pictures,
+            catagories: product.catagories
+        }).save()
     })
 
     meatRouter.get("/search/:search", async (req,res) => {
